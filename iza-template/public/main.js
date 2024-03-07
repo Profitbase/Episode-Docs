50bcba7ba1483320a942cda60a890c4a7b773f8a
Izabela Elzbieta Buczynska <ieb@profitbase.no>
2024-03-07 13:55:54 +0100

    js enhancments

---
+++ iza-template/public/main.js
@@ -0,0 +1,15 @@
+export default {
+    defaultTheme: 'dark',
+    iconLinks: [
+      {
+        icon: 'linkedin',
+        href: 'https://www.linkedin.com/company/profitbase-as/',
+        title: 'LinkedIn'
+      },
+      {
+        icon: 'globe2',
+        href: 'https://www.profitbase.com/',
+        title: 'Web'
+      }
+    ]
+  }
